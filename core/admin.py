38519e090f02d25b23184c967fad506c8e5d5886
Giorgi Maisuradze <myimeiservice@gmail.com>
2025-06-02 10:07:06 +0000

    adm

--- core/admin.py
+++
@@ -1,6 +0,0 @@
-# Version: 1.0 - 2025-05-26 - Initial admin file.
-from django.contrib import admin
-
-# Register your models here.
-# from .models import UserProfile
-# admin.site.register(UserProfile)
